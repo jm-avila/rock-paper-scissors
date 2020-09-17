ae07841d961f9047edfcf11d89a72105d6db7861
jm-avila <jm.avila1309@icloud.com>
2020-09-18 00:00:51 +0200

    Build

---
+++ build/precache-manifest.a635825bd1a65a49ad4e94b8e0626e01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310ce9a3535daa6749522379f3bc556a",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "49989d3f0f9c546220b7",
+    "url": "/rock-paper-scissors/static/css/main.87b00581.chunk.css"
+  },
+  {
+    "revision": "caf0c92d741c76d2fa53",
+    "url": "/rock-paper-scissors/static/js/2.0698661d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissors/static/js/2.0698661d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49989d3f0f9c546220b7",
+    "url": "/rock-paper-scissors/static/js/main.856e0175.chunk.js"
+  },
+  {
+    "revision": "54d8b3ee6fc233b06722",
+    "url": "/rock-paper-scissors/static/js/runtime-main.5af9a2bb.js"
+  }
+]);
